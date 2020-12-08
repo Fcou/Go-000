0164c96b352413a156f5f8d0e920435078ae45fd
Fcourage <fcou@FcouragedeMacBook-Air.local>
2020-12-08 11:11:22 +0800

    第三周作业完成

--- Week03/README.md
+++ Week03/README.md
@@ -1,1 +1,8 @@
-学习笔记
+# 学习笔记
+#### 1. [第三周课程笔记](https://naotu.baidu.com/file/0a6efe646327d237f3cac2b9c81e215f)
+#### 2. 本周问题：基于 errgroup 实现一个 http server 的启动和关闭 ，以及 linux signal 信号的注册和处理，要保证能够一个退出，全部注销退出。
+* 思路：
+    * 1.利用 errgroup 将多个 http server 并发执行
+    * 2.利用 context 的 cancel 函数管控并发执行的 http server的生命周期
+    * 3.利用 channel 监听 linux signal 信号，channel 接受到信号后，利用 cancel 函数结束全部并发http server。
+    * 4.http server 全部关闭完成后通知主 goroutine
---
+++ Week03/errgroup/errgroup.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+	shutdown := make(chan struct{})
+	// 创建带有cancel的父context
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	// 创建errgroup
+	g, _ := errgroup.WithContext(ctx)
+
+	// 并发启动server1
+	server1 := http.Server{
+		Addr: ":8081",
+	}
+	g.Go(func() error {
+		if err := server1.ListenAndServe(); err != nil {
+			cancel()
+			return err
+		}
+		return nil
+	})
+	// 并发启动server2
+	server2 := http.Server{
+		Addr: ":8082",
+	}
+	g.Go(func() error {
+		if err := server2.ListenAndServe(); err != nil {
+			cancel()
+			return err // 简单错误直接返回即可，不用pkg/errors处理
+		}
+		return nil
+	})
+
+	// 并发执行监听signal信号
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		for {
+			select {
+			case s := <-c:
+				switch s {
+				case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
+					cancel()
+				default:
+				}
+			}
+		}
+	}()
+
+	// context cancel后，关闭全部并发的 http server，全部关闭完成后通知主goroutine
+	go func() {
+		select {
+		case <-ctx.Done():
+			log.Println(ctx.Err())
+			server1.Shutdown(context.Background())
+			server1.Close()
+			server2.Shutdown(context.Background())
+			server2.Close()
+			shutdown <- struct{}{}
+			return
+		}
+
+	}()
+
+	if err := g.Wait(); err != nil {
+		log.Println(err) //打印第一个错误
+	}
+	<-shutdown
+	log.Println("all servers shutdown sucess")
+}
